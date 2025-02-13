b7f391dd601036b75113d7613eb65a3d891559d9
Miles Simpson <miles@milessimpson.com>
2025-02-13 12:30:12 -0800

    Add SSH_AUTH_SOCK check to fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,3 +1,6 @@
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/.cargo/bin"
 starship init fish | source
+if not set -q SSH_AUTH_SOCK
+    set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
+end
